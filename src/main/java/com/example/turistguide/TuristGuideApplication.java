038b5cd78adfffc24ce72492adb1aeee8c827982
Aleksandergreg <alek3691@stud.kea.dk>
2024-02-06 10:57:21 +0100

    Initial commit

---
+++ src/main/java/com/example/turistguide/TuristGuideApplication.java
@@ -0,0 +1,13 @@
+package com.example.turistguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuristGuideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TuristGuideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/turistguide/TuristGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.turistguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuristGuideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
