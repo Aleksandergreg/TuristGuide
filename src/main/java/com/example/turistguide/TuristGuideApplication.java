2293eb5547f8bf2694ca7aeb54c7f981cfc01ad6
Aleksandergreg <alek3691@stud.kea.dk>
2024-02-09 15:05:43 +0100

    Updated delete method

--- src/main/java/com/example/turistguide/TuristGuideApplication.java
+++ src/main/java/com/example/turistguide/TuristGuideApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TuristGuideApplication {
+public class    TuristGuideApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TuristGuideApplication.class, args);
